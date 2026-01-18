2685b99bf6eed09a5049e03821d6afccc9d253b4
A.P.N.Perera <132426742+pamidi09@users.noreply.github.com>
2026-01-18 13:13:40 +0530

    voice controller added

--- lib/esp32_services.dart
+++ lib/esp32_services.dart
@@ -1,17 +1,23 @@
+// lib/esp32_service.dart
 import 'package:http/http.dart' as http;
 
 class ESP32Service {
   final String espIp;
-
   ESP32Service(this.espIp);
 
-  // Forward / Backward
+  // Forward
   Future<void> moveForward(double value) async {
     final url = Uri.parse('http://$espIp/?fader1=$value');
     await http.get(url);
   }
 
-  // Left / Right
+  // Backward
+  Future<void> moveBackward(double value) async {
+    final url = Uri.parse('http://$espIp/?fader1=${1 - value}');
+    await http.get(url);
+  }
+
+  // Left/Right
   Future<void> turn(double value) async {
     final url = Uri.parse('http://$espIp/?fader2=$value');
     await http.get(url);
